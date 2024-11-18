cadbce60a9ed15f701998bb8a06e01e9b95dfc1b
petrhrabina <petr.hrabina@gmail.com>
2024-11-18 10:04:55 +0100

    remove junk

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,7 @@
-export * from './Assert.ts';
-export * from './Builder.ts';
-export * from './Preset.ts';
-export * from './Quantifiers.ts';
-export * from './Test.ts';
+import * as Assert from './Assert.ts';
+import * as Builder from './Builder.ts';
+import * as Preset from './Preset.ts';
+import * as Quantifiers from './Quantifiers.ts';
+import * as Test from './Test.ts';
+
+export { Assert, Builder, Preset, Quantifiers, Test };
